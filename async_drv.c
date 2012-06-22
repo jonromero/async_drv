0c9fc4a33e95177f323e4c02dc4ca4a1804ecd77
Jon Vlachoyiannis <jon@sfalma.com>
2012-06-21 17:44:32 -0700

    async calls

--- async_drv.c
+++ async_drv.c
@@ -2,16 +2,25 @@
 #include "erl_driver.h"
 #include "ei.h"
 
-#define PUT   'p'
+#define PUT       'p'
 #define PROCESS   'r'
 
 typedef struct _async_drv_t {
 	ErlDrvPort port;
 } async_drv_t;
 
+typedef struct _process_data_t {
+	char command;
+	char dbkey[256];
+	char value[256];
+} process_data_t;
+
 static ErlDrvData start(ErlDrvPort port, char* cmd);
 static void stop(ErlDrvData handle);
-static void process(ErlDrvData handle, ErlIOVec *ev);
+static void process(void *process_data);
+static void process_async(ErlDrvData handle, ErlIOVec *ev);
+static void ready_async(ErlDrvData drv_data, ErlDrvThreadData async_data);
+
 
 static ErlDrvEntry async_driver_entry = {
     NULL,                             /* init */
@@ -25,8 +34,8 @@ static ErlDrvEntry async_driver_entry = {
     NULL,                             /* handle */
     NULL,                             /* control */
     NULL,                             /* timeout */
-    process,                          /* outputv */
-    NULL,                             /* ready_async */
+    process_async,                    /* outputv */
+    ready_async,                             /* ready_async */
     NULL,                             /* flush */
     NULL,                             /* call */
     NULL,                             /* event */
@@ -54,46 +63,49 @@ static void stop(ErlDrvData handle) {
 	driver_free(async_drv);
 }
 
-static void process(ErlDrvData handle, ErlIOVec *ev) {
-	char command;
-	char dbkey[256];
-	char value[256];
+static void process(void *data) {
+	process_data_t *process_data = data;
+
+	switch(process_data->command) {
+	case PUT: 
+		printf("put\n");
+		break;
+	case PROCESS: // Reduces data
+		printf ("process\n");
+		break;
+	}
+}
+
+static void process_async(ErlDrvData handle, ErlIOVec *ev) {
 	async_drv_t* driver_data = (async_drv_t*) handle;
+	process_data_t *process_data = malloc(sizeof(process_data_t));
+
 	//ErlDrvBinary* data = ev->binv[1];
 	SysIOVec *iov = &ev->iov[1];;
 	const char *buf = &iov->iov_base[1];
 	int index = 0;
-	char result[1024] = "\0";
 
 	ei_decode_version(buf, &index, NULL);
 	/* Our marshalling spec is that we are expecting a tuple {Command, Arg1, Arg2} */
 	ei_decode_tuple_header(buf, &index, NULL);
 
 	// command
-	ei_decode_char(buf, &index, &command);
+	ei_decode_char(buf, &index, &process_data->command);
 	// key
-	ei_decode_string(buf, &index, dbkey);
+	ei_decode_string(buf, &index, process_data->dbkey);
 	// value
-	ei_decode_string(buf, &index, value);
+	ei_decode_string(buf, &index, process_data->value);
 
-	//printf("dbkey %s\n", dbkey);
-	//printf("value %s\n", value);
-	//printf("Command %c\n", command);
+	driver_async(driver_data->port, NULL, process, process_data, NULL);
+}
 
-	switch(command) {
-	case PUT: // puts data in calcMemory
-		printf ("put\n");
-		//put_data(driver_data->lethe, dbkey, value);
-		return;
-	case PROCESS: // Reduces data
-		printf ("process\n");
-		break;
-	}
-
+static void ready_async(ErlDrvData handle, ErlDrvThreadData async_data) {
 	ErlDrvTermData spec[] = {ERL_DRV_ATOM, driver_mk_atom((char *) "ok"),
-							 ERL_DRV_STRING, (ErlDrvTermData)result, strlen(result),
-							 ERL_DRV_TUPLE, 2};
+							 ERL_DRV_TUPLE, 1};
 
+	async_drv_t* driver_data = (async_drv_t*) handle;
+
+	// TODO: free process_t from async_data
 	driver_output_term(driver_data->port, spec, sizeof(spec) / sizeof(spec[0]));
 }
 
